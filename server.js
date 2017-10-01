1129c25ffcae6c9ab66dd33b4bb9d7db7507f97b
piq9117 <piq9117@gmail.com>
2017-09-30 22:27:16 -0500

    deleted Verses import, not needed

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const Verses = require("./models/verse");
 const routes = require("./routes");
 const logger = require("morgan");
 
